977e52242f71071d1b610672ac62b29955e9c664
gongdaoyuito <83847483+gongdaoyuito@users.noreply.github.com>
2021-06-30 14:44:48 +0900

    Update kuizu.js

--- kuizu.js
+++ kuizu.js
@@ -381,3 +381,556 @@ $(function(){
      }
    });
 });
+
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#kyuu").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main").click(function(){
+          $("#modal-main,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main").outerWidth();
+           var ch = $("#modal-main").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#zyuu").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main").click(function(){
+          $("#modal-main,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main").outerWidth();
+           var ch = $("#modal-main").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#zyuuiti").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file2' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main2").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main2").click(function(){
+          $("#modal-main2,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main2").outerWidth();
+           var ch = $("#modal-main2").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main2").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#zyuuni").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main").click(function(){
+          $("#modal-main,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main").outerWidth();
+           var ch = $("#modal-main").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#zyuusan").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main").click(function(){
+          $("#modal-main,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main").outerWidth();
+           var ch = $("#modal-main").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#zyuuyon").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main").click(function(){
+          $("#modal-main,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main").outerWidth();
+           var ch = $("#modal-main").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#zyuugo").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main").click(function(){
+          $("#modal-main,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main").outerWidth();
+           var ch = $("#modal-main").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#zyuuroku").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file2' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main2").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main2").click(function(){
+          $("#modal-main2,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main2").outerWidth();
+           var ch = $("#modal-main2").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main2").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#zyuunana").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file2' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main2").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main2").click(function(){
+          $("#modal-main2,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main2").outerWidth();
+           var ch = $("#modal-main2").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main2").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#zyuuhati").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main").click(function(){
+          $("#modal-main,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main").outerWidth();
+           var ch = $("#modal-main").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#zyuukyuu").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main").click(function(){
+          $("#modal-main,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main").outerWidth();
+           var ch = $("#modal-main").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
+//modal
+$(function(){
+ 
+  //テキストリンクをクリックしたら
+ $("#nizyuu").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+     $("body").append('<div id="modal-bg"></div>');
+ 
+    //画面中央を計算する関数を実行
+    modalResize();
+ 
+    //モーダルウィンドウを表示
+        function sound()
+        {
+          // [ID:sound-file]の音声ファイルを再生[play()]する
+          $( '#sound-file' ).get(0).play() ;
+        }
+        $("#modal-bg,#modal-main").fadeIn("slow");
+ 
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#modal-bg,#modal-main").click(function(){
+          $("#modal-main,#modal-bg").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+              $('#modal-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#modal-main").outerWidth();
+           var ch = $("#modal-main").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#modal-main").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
+});
